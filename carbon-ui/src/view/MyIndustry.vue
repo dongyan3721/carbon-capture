4b76e8b6eb6c0b24bfd46ccd5629f08bb5acbc2f
David Antillies <93983691+dongyan3721@users.noreply.github.com>
2024-02-22 14:57:53 +0800

    推荐页 结束

--- carbon-ui/src/view/MyIndustry.vue
+++ carbon-ui/src/view/MyIndustry.vue
@@ -62,10 +62,10 @@
         <el-main>
           <el-form :model="myBelongForm" :inline="false" ref="myBelongFormRef" :rules="myBelongFormRules">
             <el-form-item label="我的行业" prop="industry">
-              <el-cascader :options="dict_industry_belong" @change="currentlyChangeFormData" clearable/>
+              <el-cascader :options="dict_industry_belong" placeholder="请选择行业与行业环节" @change="currentlyChangeFormData" clearable/>
             </el-form-item>
             <el-form-item label="我的地理位置" prop="provinceId">
-              <el-select v-model="myBelongForm.provinceId" clearable>
+              <el-select v-model="myBelongForm.provinceId" clearable placeholder="请选择省份">
                 <el-option v-for="province in provinces"
                            :value="province.value"
                            :key="province.value"
@@ -73,13 +73,28 @@
               </el-select>
             </el-form-item>
             <el-form-item label="年均大致碳排放量" prop="blurCarbonOutput">
-              <el-input v-model="myBelongForm.blurCarbonOutput" clearable/>
+              <el-input v-model="myBelongForm.blurCarbonOutput" clearable placeholder="单位：吨"/>
             </el-form-item>
             <el-form-item>
               <el-button type="danger" @click="submitMyBelongForm(myBelongFormRef)">提交</el-button>
-              <el-button type="primary" @click="resetMyBelongForm">重置</el-button>
+              <el-button type="primary" @click="cancelMyBelongForm">重置</el-button>
             </el-form-item>
           </el-form>
+          <hr/>
+          <p class="recommend-hint">根据您的行业，为您推荐以下可行捕集方案</p>
+          <el-table :data="solutionTableData" v-loading="solutionTableDataLoading" :border="true">
+            <template #empty>
+              <p class="no-data-hint">请选择行业后再行查看</p>
+            </template>
+            <el-table-column prop="method" label="捕集方法"/>
+            <el-table-column prop="gasOutput" label="烟气排量"/>
+            <el-table-column prop="captureEnergyExpenditure" label="捕集能耗"/>
+            <el-table-column prop="referenceEssay" label="下载查看">
+              <template #default="scope">
+                <el-button text @click="downloadEssay(scope.row)" size="small">下载</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-main>
       </el-container>
     </el-container>
@@ -92,7 +107,13 @@ import {ArrowRight, User, Edit, Finished} from "@element-plus/icons-vue";
 import {onBeforeMount, reactive, ref} from "vue";
 import {getLocalStorage, KEY_AVATAR, KEY_NICKNAME, KEY_USER_ID} from "@/utils/localStorge";
 import {baseStaticRecourseAPI, NOW_ENVIRONMENT} from "@/config/baseAPIConfig";
-import {requestAllProvinces, requestRecommendedCarbonSolution, requestUserBelongDetail} from "@/web-api/center-extend";
+import {
+  addUserBelong,
+  modifyUserBelong,
+  requestAllProvinces,
+  requestRecommendedCarbonSolution,
+  requestUserBelongDetail
+} from "@/web-api/center-extend";
 import {ElMessage} from "element-plus";
 import {dict_industry_belong, generalNumericValidator, generalValidatorJudgeIfEmpty} from "@/utils/common";
 
@@ -102,7 +123,20 @@ const userId = getLocalStorage(KEY_USER_ID);
 const submitMyBelongForm = (fromRef)=>{
   fromRef.validate(valid=>{
     if(valid){
-
+      myBelongForm.provinceName = provinceReflect[myBelongForm.provinceId];
+      if(myBelongForm.belongId){
+        modifyUserBelong(myBelongForm).then(res=>{
+          ElMessage.success('修改成功！')
+          solutionTableDataLoading.value = true;
+          requestSpecificSolution(userId);
+        })
+      }else{
+        addUserBelong(myBelongForm).then(res=>{
+          ElMessage.success('记录添加成功！');
+          solutionTableDataLoading.value = true
+          requestUserBelong(false);
+        })
+      }
     }
   })
 }
@@ -124,6 +158,10 @@ let myBelongForm = reactive({
   industryProcess: null,
   blurCarbonOutput: null
 })
+const cancelMyBelongForm = ()=>{
+  resetMyBelongForm();
+  requestUserBelong(false);
+}
 const resetMyBelongForm = ()=>{
   if(!myBelongFormRef.value){
     return
@@ -154,11 +192,16 @@ let myBelongFormRef = ref();
 let provinces = ref([]);
 // 省份名和id的映射
 let provinceReflect = reactive({})
+
+
 // 存储推荐的解决方案
 let solutionTableData = ref([])
 // python跑模型可能比较慢，遮罩一下解决方案的表格
 let solutionTableDataLoading = ref(false);
+// 下载参考方法
+const downloadEssay = (row)=>{
 
+}
 
 
 // 页面加载时要干的事
@@ -173,6 +216,19 @@ function checkWhetherUserHadChosenHisBelong(){
       provinceReflect[province.id] = province.regionName
     })
   })
+  requestUserBelong(true);
+}
+
+// 请求解决方案
+function requestSpecificSolution(userId){
+  requestRecommendedCarbonSolution(userId).then(res=>{
+    solutionTableData.value = res.rows;
+    solutionTableDataLoading.value = false;
+  }).catch(err=>console.error('network error', err))
+}
+
+// 请求用户行业，参数是是否需要提醒用户选择
+function requestUserBelong(requireRemind){
   requestUserBelongDetail(userId).then(res=>{
     if(res.data){
       myBelongForm.belongId = res.data.belongId
@@ -184,13 +240,12 @@ function checkWhetherUserHadChosenHisBelong(){
       myBelongForm.industryProcess = res.data.industryProcess
       myBelongForm.blurCarbonOutput = res.data.blurCarbonOutput
       // 填了行业信息，那么继续去请求对应的解决方案
-      requestRecommendedCarbonSolution(userId).then(res=>{
-        solutionTableData.value = res.rows;
-        solutionTableDataLoading.value = false;
-      }).catch(err=>console.error('network error', err))
+      requestSpecificSolution(userId);
     }else{
-      ElMessage.warning('请先完成行业信息录入再使用本功能！')
-      solutionTableDataLoading.value = false
+      if(requireRemind){
+        ElMessage.warning('请先在本页面完成行业信息录入再使用本功能！')
+        solutionTableDataLoading.value = false
+      }
     }
   })
 }
@@ -222,5 +277,18 @@ let avatarSrc = baseStaticRecourseAPI[NOW_ENVIRONMENT]+getLocalStorage(KEY_AVATA
 
 .el-menu-item:hover {
   border-radius: 20px;
+}
+.recommend-hint{
+  font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+  font-weight: normal;
+  color: #606266;
+  font-size: 14px;
+  cursor: default;
+}
+.no-data-hint{
+  font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+  color: #a8abb2;
+  font-size: 18px;
+  user-select: none;
 }
 </style>
--- sql/bulid.sql
+++ sql/bulid.sql
@@ -13,10 +13,10 @@ create table t_provinces
 
 create table t_carbon_user
 (
-    user_id varchar(36) not null primary key ,
-    user_type char(1) not null comment '0普通用户1网站管理员',
-    email varchar(24) not null comment '登录邮箱',
-    nickname varchar(64) not null ,
+    user_id     varchar(36)                        not null primary key,
+    user_type   char(1)                            not null comment '0普通用户1网站管理员',
+    email       varchar(24)                        not null comment '登录邮箱',
+    nickname    varchar(64)                        not null,
     create_time datetime default CURRENT_TIMESTAMP null,
     create_by   varchar(255)                       null,
     modify_time datetime                           null,
@@ -26,43 +26,43 @@ create table t_carbon_user
 
 create table t_carbon_user_belong
 (
-    belong_id varchar(36) not null primary key ,
-    user_id varchar(36) not null comment '用户id',
-    user_nickname varchar(64) not null comment '用户昵称-冗',
-    province_id varchar(36) null comment '所在省份',
-    province_name varchar(50) null comment '省份名',
-    industry varchar(64) null comment '所在行业',
-    industry_process varchar(64) null comment '行业环节',
+    belong_id          varchar(36)                        not null primary key,
+    user_id            varchar(36)                        not null comment '用户id',
+    user_nickname      varchar(64)                        not null comment '用户昵称-冗',
+    province_id        varchar(36)                        null comment '所在省份',
+    province_name      varchar(50)                        null comment '省份名',
+    industry           varchar(64)                        null comment '所在行业',
+    industry_process   varchar(64)                        null comment '行业环节',
     blur_carbon_output decimal(16, 2) comment '大致年度碳排量',
-    create_time datetime default CURRENT_TIMESTAMP null,
-    create_by   varchar(255)                       null,
-    modify_time datetime                           null,
-    modify_by   varchar(255)                       null,
-    remark      varchar(1024)                      null
+    create_time        datetime default CURRENT_TIMESTAMP null,
+    create_by          varchar(255)                       null,
+    modify_time        datetime                           null,
+    modify_by          varchar(255)                       null,
+    remark             varchar(1024)                      null
 #     foreign key (province_id) references t_provinces(id)
 );
 
 create table t_province_carbon_output
 (
-    output_id varchar(36) not null primary key ,
-    province_id varchar(36) not null comment '省份id',
-    province_name varchar(50) not null comment '省份名',
-    output decimal(16, 2) default 0 comment '碳排量，单位吨',
-    year varchar(4) not null comment '统计年份',
-    create_time datetime default CURRENT_TIMESTAMP null,
-    create_by   varchar(255)                       null,
-    modify_time datetime                           null,
-    modify_by   varchar(255)                       null,
-    remark      varchar(1024)                      null
+    output_id     varchar(36)                              not null primary key,
+    province_id   varchar(36)                              not null comment '省份id',
+    province_name varchar(50)                              not null comment '省份名',
+    output        decimal(16, 2) default 0 comment '碳排量，单位吨',
+    year          varchar(4)                               not null comment '统计年份',
+    create_time   datetime       default CURRENT_TIMESTAMP null,
+    create_by     varchar(255)                             null,
+    modify_time   datetime                                 null,
+    modify_by     varchar(255)                             null,
+    remark        varchar(1024)                            null
 #     foreign key (province_id) references t_provinces(id)
 );
 
 create table t_province_policy
 (
-    policy_id varchar(36) not null primary key ,
-    title varchar(1024) not null ,
-    url varchar(1024) not null comment '内容链接',
-    province_id varchar(36) not null comment '省份id',
+    policy_id   varchar(36)                        not null primary key,
+    title       varchar(1024)                      not null,
+    url         varchar(1024)                      not null comment '内容链接',
+    province_id varchar(36)                        not null comment '省份id',
     create_time datetime default CURRENT_TIMESTAMP null,
     create_by   varchar(255)                       null,
     modify_time datetime                           null,
@@ -72,10 +72,10 @@ create table t_province_policy
 
 create table t_province_intent
 (
-    intent_id varchar(36) not null primary key ,
-    title varchar(1024) not null ,
-    url varchar(1024) not null comment '内容链接',
-    province_id varchar(36) not null comment '省份id',
+    intent_id   varchar(36)                        not null primary key,
+    title       varchar(1024)                      not null,
+    url         varchar(1024)                      not null comment '内容链接',
+    province_id varchar(36)                        not null comment '省份id',
     create_time datetime default CURRENT_TIMESTAMP null,
     create_by   varchar(255)                       null,
     modify_time datetime                           null,
@@ -85,10 +85,10 @@ create table t_province_intent
 
 create table t_province_case
 (
-    case_id varchar(36) not null primary key ,
-    title varchar(1024) not null ,
-    url varchar(1024) not null comment '内容链接',
-    province_id varchar(36) not null comment '省份id',
+    case_id     varchar(36)                        not null primary key,
+    title       varchar(1024)                      not null,
+    url         varchar(1024)                      not null comment '内容链接',
+    province_id varchar(36)                        not null comment '省份id',
     create_time datetime default CURRENT_TIMESTAMP null,
     create_by   varchar(255)                       null,
     modify_time datetime                           null,
@@ -98,14 +98,36 @@ create table t_province_case
 
 create table t_province_news
 (
-    news_id varchar(36) not null primary key ,
-    title varchar(1024) not null ,
-    content_url varchar(1024) not null comment '内容链接',
-    image_url varchar(1024) not null comment '预览图URL',
-    province_id varchar(36) not null comment '省份id',
+    news_id     varchar(36)                        not null primary key,
+    title       varchar(1024)                      not null,
+    content_url varchar(1024)                      not null comment '内容链接',
+    image_url   varchar(1024)                      not null comment '预览图URL',
+    province_id varchar(36)                        not null comment '省份id',
     create_time datetime default CURRENT_TIMESTAMP null,
     create_by   varchar(255)                       null,
     modify_time datetime                           null,
     modify_by   varchar(255)                       null,
     remark      varchar(1024)                      null
 ) comment '省份双碳新闻表';
+
+create table t_solution
+(
+    case_id                    varchar(36)                        not null
+        primary key,
+    title                      varchar(1024)                      null,
+    industry                   varchar(255)                       not null comment '行业',
+    industry_process           varchar(255)                       null comment '行业环节',
+    method                     varchar(512)                       not null comment '方法',
+    gas_output                 varchar(255)                       null comment '烟气排量',
+    gas_comprise_temperature   varchar(255)                       null comment '烟气组成',
+    capture_ratio              varchar(255)                       null comment '捕集率',
+    capture_energy_expenditure varchar(255)                       null comment '捕集能耗',
+    method_usage               varchar(255)                       null comment '用途',
+    reference_essay            varchar(1024)                      null comment '可供参考的文章',
+    create_time                datetime default CURRENT_TIMESTAMP null,
+    create_by                  varchar(255)                       null,
+    modify_time                datetime                           null,
+    modify_by                  varchar(255)                       null,
+    remark                     varchar(1024)                      null
+)
+    comment '已有的捕集方法表';
